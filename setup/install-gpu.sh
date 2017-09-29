4b578df3636dafe285707cb6d9cdb96fe7bf6142
Ying Huang <yhuang@imvu.com>
2017-09-28 19:04:37 -0700

    n

--- setup/install-gpu.sh
+++ setup/install-gpu.sh
@@ -1,25 +1,36 @@
+#!/bin/bash - 
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
 # This script is designed to work with ubuntu 16.04 LTS
 
 # ensure system is updated and has basic build tools
-sudo apt-get update
-sudo apt-get --assume-yes upgrade
-sudo apt-get --assume-yes install tmux build-essential gcc g++ make binutils
-sudo apt-get --assume-yes install software-properties-common
+apt-get update
+apt-get --assume-yes upgrade
+apt-get --assume-yes install tmux build-essential gcc g++ make binutils
+apt-get --assume-yes install software-properties-common
 
 # download and install GPU drivers
-wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb" -O "cuda-repo-ubuntu1604_8.0.44-1_amd64.deb"
+if [ ! -f "cuda-repo-ubuntu1604_8.0.44-1_amd64.deb" ]
+then
+    wget "http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.44-1_amd64.deb" -O "cuda-repo-ubuntu1604_8.0.44-1_amd64.deb"
+fi 
 
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
-sudo apt-get update
-sudo apt-get -y install cuda
-sudo modprobe nvidia
+dpkg -i cuda-repo-ubuntu1604_8.0.44-1_amd64.deb
+apt-get update
+apt-get -y install cuda
+modprobe nvidia
 nvidia-smi
 
 # install Anaconda for current user
 mkdir downloads
 cd downloads
-wget "https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh" -O "Anaconda2-4.2.0-Linux-x86_64.sh"
-bash "Anaconda2-4.2.0-Linux-x86_64.sh" -b
+if [ ! -f "Anaconda2-4.2.0-Linux-x86_64.sh" ]
+then
+    wget "https://repo.continuum.io/archive/Anaconda2-4.2.0-Linux-x86_64.sh" -O "Anaconda2-4.2.0-Linux-x86_64.sh"
+fi 
+bash "Anaconda2-4.2.0-Linux-x86_64.sh" -b -p /media/volgrp/anaconda2
 
 echo "export PATH=\"$HOME/anaconda2/bin:\$PATH\"" >> ~/.bashrc
 export PATH="$HOME/anaconda2/bin:$PATH"
@@ -46,11 +57,14 @@ echo '{
 }' > ~/.keras/keras.json
 
 # install cudnn libraries
-wget "http://files.fast.ai/files/cudnn.tgz" -O "cudnn.tgz"
+if [ ! -f "cudnn.tgz" ]
+then
+    wget "http://files.fast.ai/files/cudnn.tgz" -O "cudnn.tgz"
+fi 
 tar -zxf cudnn.tgz
 cd cuda
-sudo cp lib64/* /usr/local/cuda/lib64/
-sudo cp include/* /usr/local/cuda/include/
+cp lib64/* /usr/local/cuda/lib64/
+cp include/* /usr/local/cuda/include/
 
 # configure jupyter and prompt for password
 jupyter notebook --generate-config
