853581f20dc401501c603e57646c6e99c60df52d
Ying Huang <yhuang@imvu.com>
2017-10-26 19:32:33 -0700

    n

--- deeplearning1/nbs/statefarm-sample.py
+++ deeplearning1/nbs/statefarm-sample.py
@@ -17,7 +17,8 @@ get_ipython().magic(u'matplotlib inline')
 from __future__ import print_function, division
 #path = "data/state/"
 path = "data/state/sample/"
-import utils; reload(utils)
+import utils
+reload(utils)
 from utils import *
 from IPython.display import FileLink
 
@@ -25,7 +26,7 @@ from IPython.display import FileLink
 # In[3]:
 
 
-batch_size=64
+batch_size = 64
 
 
 # ## Create sample
@@ -56,8 +57,8 @@ get_ipython().magic(u'mkdir ../sample/valid')
 
 
 for d in glob('c?'):
-    os.mkdir('../sample/train/'+d)
-    os.mkdir('../sample/valid/'+d)
+    os.mkdir('../sample/train/' + d)
+    os.mkdir('../sample/valid/' + d)
 
 
 # In[ ]:
@@ -71,7 +72,8 @@ from shutil import copyfile
 
 g = glob('c?/*.jpg')
 shuf = np.random.permutation(g)
-for i in range(1500): copyfile(shuf[i], '../sample/train/' + shuf[i])
+for i in range(1500):
+    copyfile(shuf[i], '../sample/train/' + shuf[i])
 
 
 # In[ ]:
@@ -85,7 +87,8 @@ get_ipython().magic(u'cd ../valid')
 
 g = glob('c?/*.jpg')
 shuf = np.random.permutation(g)
-for i in range(1000): copyfile(shuf[i], '../sample/valid/' + shuf[i])
+for i in range(1000):
+    copyfile(shuf[i], '../sample/valid/' + shuf[i])
 
 
 # In[ ]:
@@ -111,8 +114,9 @@ get_ipython().magic(u'mkdir data/state/sample/test')
 # In[56]:
 
 
-batches = get_batches(path+'train', batch_size=batch_size)
-val_batches = get_batches(path+'valid', batch_size=batch_size*2, shuffle=False)
+batches = get_batches(path + 'train', batch_size=batch_size)
+val_batches = get_batches(
+    path + 'valid', batch_size=batch_size * 2, shuffle=False)
 
 
 # In[5]:
@@ -132,10 +136,10 @@ val_batches = get_batches(path+'valid', batch_size=batch_size*2, shuffle=False)
 
 
 model = Sequential([
-        BatchNormalization(axis=1, input_shape=(3,224,224)),
-        Flatten(),
-        Dense(10, activation='softmax')
-    ])
+    BatchNormalization(axis=1, input_shape=(3, 224, 224)),
+    Flatten(),
+    Dense(10, activation='softmax')
+])
 
 
 # As you can see below, this training is going nowhere...
@@ -144,8 +148,8 @@ model = Sequential([
 
 
 model.compile(Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # Let's first check the number of parameters to see that there's enough parameters to find some useful relationships:
@@ -161,7 +165,7 @@ model.summary()
 # In[67]:
 
 
-10*3*224*224
+10 * 3 * 224 * 224
 
 
 # Since we have a simple model with no regularization and plenty of parameters, it seems most likely that our learning rate is too high. Perhaps it is jumping to a solution where it predicts one or two classes with high confidence, so that it can give a zero prediction to as many classes as possible - that's the best approach for a model that is no better than random, and there is likely to be where we would end up with a high learning rate. So let's check:
@@ -169,7 +173,7 @@ model.summary()
 # In[10]:
 
 
-np.round(model.predict_generator(batches, batches.N)[:10],2)
+np.round(model.predict_generator(batches, batches.N)[:10], 2)
 
 
 # Our hypothesis was correct. It's nearly always predicting class 1 or 6, with very high confidence. So let's try a lower learning rate:
@@ -178,13 +182,14 @@ np.round(model.predict_generator(batches, batches.N)[:10],2)
 
 
 model = Sequential([
-        BatchNormalization(axis=1, input_shape=(3,224,224)),
-        Flatten(),
-        Dense(10, activation='softmax')
-    ])
-model.compile(Adam(lr=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+    BatchNormalization(axis=1, input_shape=(3, 224, 224)),
+    Flatten(),
+    Dense(10, activation='softmax')
+])
+model.compile(Adam(lr=1e-5), loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # Great - we found our way out of that hole... Now we can increase the learning rate and see where we can get to.
@@ -192,14 +197,14 @@ model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_
 # In[15]:
 
 
-model.optimizer.lr=0.001
+model.optimizer.lr = 0.001
 
 
 # In[16]:
 
 
-model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # We're stabilizing at validation accuracy of 0.39. Not great, but a lot better than random. Before moving on, let's check that our validation set on the sample is large enough that it gives consistent results:
@@ -207,13 +212,15 @@ model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_
 # In[6]:
 
 
-rnd_batches = get_batches(path+'valid', batch_size=batch_size*2, shuffle=True)
+rnd_batches = get_batches(
+    path + 'valid', batch_size=batch_size * 2, shuffle=True)
 
 
 # In[11]:
 
 
-val_res = [model.evaluate_generator(rnd_batches, rnd_batches.nb_sample) for i in range(10)]
+val_res = [model.evaluate_generator(
+    rnd_batches, rnd_batches.nb_sample) for i in range(10)]
 np.round(val_res, 2)
 
 
@@ -227,26 +234,27 @@ np.round(val_res, 2)
 
 
 model = Sequential([
-        BatchNormalization(axis=1, input_shape=(3,224,224)),
-        Flatten(),
-        Dense(10, activation='softmax', W_regularizer=l2(0.01))
-    ])
-model.compile(Adam(lr=10e-5), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+    BatchNormalization(axis=1, input_shape=(3, 224, 224)),
+    Flatten(),
+    Dense(10, activation='softmax', W_regularizer=l2(0.01))
+])
+model.compile(Adam(lr=10e-5), loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # In[21]:
 
 
-model.optimizer.lr=0.001
+model.optimizer.lr = 0.001
 
 
 # In[22]:
 
 
-model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # Looks like we can get a bit over 50% accuracy this way. This will be a good benchmark for our future models - if we can't beat 50%, then we're not even beating a linear model trained on a sample, so we'll know that's not a good approach.
@@ -259,19 +267,20 @@ model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_
 
 
 model = Sequential([
-        BatchNormalization(axis=1, input_shape=(3,224,224)),
-        Flatten(),
-        Dense(100, activation='relu'),
-        BatchNormalization(),
-        Dense(10, activation='softmax')
-    ])
-model.compile(Adam(lr=1e-5), loss='categorical_crossentropy', metrics=['accuracy'])
-model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+    BatchNormalization(axis=1, input_shape=(3, 224, 224)),
+    Flatten(),
+    Dense(100, activation='relu'),
+    BatchNormalization(),
+    Dense(10, activation='softmax')
+])
+model.compile(Adam(lr=1e-5), loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 model.optimizer.lr = 0.01
-model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # Not looking very encouraging... which isn't surprising since we know that CNNs are a much better choice for computer vision problems. So we'll try one.
@@ -285,25 +294,26 @@ model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_
 
 def conv1(batches):
     model = Sequential([
-            BatchNormalization(axis=1, input_shape=(3,224,224)),
-            Convolution2D(32,3,3, activation='relu'),
-            BatchNormalization(axis=1),
-            MaxPooling2D((3,3)),
-            Convolution2D(64,3,3, activation='relu'),
-            BatchNormalization(axis=1),
-            MaxPooling2D((3,3)),
-            Flatten(),
-            Dense(200, activation='relu'),
-            BatchNormalization(),
-            Dense(10, activation='softmax')
-        ])
+        BatchNormalization(axis=1, input_shape=(3, 224, 224)),
+        Convolution2D(32, 3, 3, activation='relu'),
+        BatchNormalization(axis=1),
+        MaxPooling2D((3, 3)),
+        Convolution2D(64, 3, 3, activation='relu'),
+        BatchNormalization(axis=1),
+        MaxPooling2D((3, 3)),
+        Flatten(),
+        Dense(200, activation='relu'),
+        BatchNormalization(),
+        Dense(10, activation='softmax')
+    ])
 
-    model.compile(Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches, 
-                     nb_val_samples=val_batches.nb_sample)
+    model.compile(Adam(lr=1e-4), loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=2, validation_data=val_batches,
+                        nb_val_samples=val_batches.nb_sample)
     model.optimizer.lr = 0.001
-    model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches, 
-                     nb_val_samples=val_batches.nb_sample)
+    model.fit_generator(batches, batches.nb_sample, nb_epoch=4, validation_data=val_batches,
+                        nb_val_samples=val_batches.nb_sample)
     return model
 
 
@@ -314,7 +324,7 @@ conv1(batches)
 
 
 # The training set here is very rapidly reaching a very high accuracy. So if we could regularize this, perhaps we could get a reasonable result.
-# 
+#
 # So, what kind of regularization should we try first? As we discussed in lesson 3, we should start with data augmentation.
 
 # ## Data augmentation
@@ -327,7 +337,7 @@ conv1(batches)
 
 
 gen_t = image.ImageDataGenerator(width_shift_range=0.1)
-batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+batches = get_batches(path + 'train', gen_t, batch_size=batch_size)
 
 
 # In[64]:
@@ -342,7 +352,7 @@ model = conv1(batches)
 
 
 gen_t = image.ImageDataGenerator(height_shift_range=0.05)
-batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+batches = get_batches(path + 'train', gen_t, batch_size=batch_size)
 
 
 # In[66]:
@@ -357,7 +367,7 @@ model = conv1(batches)
 
 
 gen_t = image.ImageDataGenerator(shear_range=0.1)
-batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+batches = get_batches(path + 'train', gen_t, batch_size=batch_size)
 
 
 # In[68]:
@@ -372,7 +382,7 @@ model = conv1(batches)
 
 
 gen_t = image.ImageDataGenerator(rotation_range=15)
-batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+batches = get_batches(path + 'train', gen_t, batch_size=batch_size)
 
 
 # In[70]:
@@ -381,13 +391,13 @@ batches = get_batches(path+'train', gen_t, batch_size=batch_size)
 model = conv1(batches)
 
 
-# Channel shift: randomly changing the R,G,B colors - 
+# Channel shift: randomly changing the R,G,B colors -
 
 # In[76]:
 
 
 gen_t = image.ImageDataGenerator(channel_shift_range=20)
-batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+batches = get_batches(path + 'train', gen_t, batch_size=batch_size)
 
 
 # In[77]:
@@ -401,9 +411,9 @@ model = conv1(batches)
 # In[75]:
 
 
-gen_t = image.ImageDataGenerator(rotation_range=15, height_shift_range=0.05, 
-                shear_range=0.1, channel_shift_range=20, width_shift_range=0.1)
-batches = get_batches(path+'train', gen_t, batch_size=batch_size)
+gen_t = image.ImageDataGenerator(rotation_range=15, height_shift_range=0.05,
+                                 shear_range=0.1, channel_shift_range=20, width_shift_range=0.1)
+batches = get_batches(path + 'train', gen_t, batch_size=batch_size)
 
 
 # In[59]:
@@ -418,8 +428,8 @@ model = conv1(batches)
 
 
 model.optimizer.lr = 0.0001
-model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # Lucky we tried that - we starting to make progress! Let's keep going.
@@ -427,10 +437,10 @@ model.fit_generator(batches, batches.nb_sample, nb_epoch=5, validation_data=val_
 # In[61]:
 
 
-model.fit_generator(batches, batches.nb_sample, nb_epoch=25, validation_data=val_batches, 
-                 nb_val_samples=val_batches.nb_sample)
+model.fit_generator(batches, batches.nb_sample, nb_epoch=25, validation_data=val_batches,
+                    nb_val_samples=val_batches.nb_sample)
 
 
 # Amazingly, using nothing but a small sample, a simple (not pre-trained) model with no dropout, and data augmentation, we're getting results that would get us into the top 50% of the competition! This looks like a great foundation for our futher experiments.
-# 
+#
 # To go further, we'll need to use the whole dataset, since dropout and data volumes are very related, so we can't tweak dropout without using all the data.
