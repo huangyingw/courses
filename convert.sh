72da6ac7036b4a99bc6e62a18689c696696237fb
Ying Huang <yhuang@imvu.com>
2017-11-06 18:11:26 -0800

    n

--- convert.sh
+++ convert.sh
@@ -3,5 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-find . -type f -name \*.ipynb -exec jupyter nbconvert --to=python {} \;
+find . -type f -name \*.ipynb -exec jupyter nbconvert --to=python --template=python.tpl {} \;
 find . -type f -name \*.py -exec autopep8 --in-place --aggressive {} \;
