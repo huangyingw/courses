3ab99209ff8c77af2950bf483d54fe7ceb194003
Ying Huang <yhuang@imvu.com>
2017-11-15 19:38:36 -0800

    n

--- convert.sh
+++ convert.sh
@@ -3,5 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+find . -type f -name \*.ipynb -exec python ~/loadrc/pythonrc/autopep8-ipynb.py {} \;
 find . -type f -name \*.ipynb -exec jupyter nbconvert --to=python --template=python.tpl {} \;
 find ./deeplearning*/  -type f -name \*.py -exec autopep8 --in-place --aggressive {} \;
