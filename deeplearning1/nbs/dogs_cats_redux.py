1d3a0cd898d00ace1a0b7d4c10be06af2f47ab39
Ying Huang <yhuang@imvu.com>
2017-12-26 18:16:45 -0800

    n

--- deeplearning1/nbs/dogs_cats_redux.py
+++ deeplearning1/nbs/dogs_cats_redux.py
@@ -4,19 +4,14 @@ from PIL import Image
 from glob import glob
 from keras.layers.convolutional import *
 from keras.preprocessing import image
-from matplotlib import pyplot as plt
-from numpy.random import permutation
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import log_loss
 from utils import *
 from vgg16 import *
 from vgg16 import Vgg16
 from vgg16bn import *
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import shutil
-import sys
 
 # # Dogs vs Cat Redux
 
@@ -334,31 +329,6 @@ isdog = preds[:, 1]
 print "Raw Predictions: " + str(isdog[:5])
 print "Mid Predictions: " + str(isdog[(isdog < .6) & (isdog > .4)])
 print "Edge Predictions: " + str(isdog[(isdog == 1) | (isdog == 0)])
-
-
-# [Log Loss](http://wiki.fast.ai/index.php/Log_Loss) doesn't support probability values of 0 or 1--they are undefined (and we have many). Fortunately, Kaggle helps us by offsetting our 0s and 1s by a very small value. So if we upload our submission now we will have lots of .99999999 and .000000001 values. This seems good, right?
-#
-# Not so. There is an additional twist due to how log loss is
-# calculated--log loss rewards predictions that are confident and correct
-# (p=.9999,label=1), but it punishes predictions that are confident and
-# wrong far more (p=.0001,label=1). See visualization below.
-
-# Visualize Log Loss when True value = 1
-# y-axis is log loss, x-axis is probabilty that label = 1
-# As you can see Log Loss increases rapidly as we approach 0
-# But increases slowly as our predicted probability gets closer to 1
-
-x = [i * .0001 for i in range(1, 10000)]
-y = [log_loss([1, 0], [i * .0001, 1 - (i * .0001)], eps=1e-15)
-     for i in range(1, 10000, 1)]
-
-plt.plot(x, y)
-plt.axis([-.05, 1.1, -.8, 10])
-plt.title("Log Loss when true label = 1")
-plt.xlabel("predicted probability")
-plt.ylabel("log loss")
-
-plt.show()
 
 
 # So to play it safe, we use a sneaky trick to round down our edge predictions
