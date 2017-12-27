c3dbf6ba6bdab30c1677e86fc6f2853f242232da
Ying Huang <yhuang@imvu.com>
2017-12-26 23:05:08 -0800

    n

--- deeplearning1/nbs/sgd-intro.py
+++ deeplearning1/nbs/sgd-intro.py
@@ -1,17 +1,8 @@
-
-# coding: utf-8
-
-# # Table of Contents
-#  <p>
-
 import numpy as np
 from numpy.random import random
 from matplotlib import pyplot as plt, rcParams, animation, rc
-from __future__ import print_function, division
-from ipywidgets.widgets import *
 rc('animation', html='html5')
 rcParams['figure.figsize'] = 3, 3
-get_ipython().magic(u'precision 4')
 np.set_printoptions(precision=4, linewidth=100)
 
 
@@ -20,20 +11,12 @@ def lin(a, b, x): return a * x + b
 
 a = 3.
 b = 8.
-
-
 n = 30
 x = random(n)
 y = lin(a, b, x)
 
-
-x
-
-
-y
-
-
 plt.scatter(x, y)
+plt.show()
 
 
 def sse(y, y_pred): return ((y - y_pred)**2).sum()
@@ -48,7 +31,6 @@ def avg_loss(y, a, b, x): return np.sqrt(loss(y, a, b, x) / n)
 a_guess = -1.
 b_guess = 1.
 avg_loss(y, a_guess, b_guess, x)
-
 
 lr = 0.01
 # d[(y-(a*x+b))**2,b] = 2 (b + a x - y)      = 2 (y_pred - y)
@@ -67,6 +49,7 @@ def upd():
 fig = plt.figure(dpi=100, figsize=(5, 4))
 plt.scatter(x, y)
 line, = plt.plot(x, lin(a_guess, b_guess, x))
+plt.show()
 plt.close()
 
 
